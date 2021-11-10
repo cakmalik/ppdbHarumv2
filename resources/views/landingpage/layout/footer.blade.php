fbcebf80f5d1ef3746332d2d9309f2bcdae5631b
x <x@xs-MacBook-Pro.local>
2021-11-10 13:38:03 +0700

    penambahan maintenance mode

--- resources/views/landingpage/layout/footer.blade.php
+++ resources/views/landingpage/layout/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start">
-                    <p>Copyright © SDIT Harapan Umat Jember 2021-2022 by PM & IAI</p>
+                    <p>Copyright © SDIT Harum Jember</p>
                 </div>
                 <div class="col-md-6 text-center text-md-end mb-4">
                     <a href="https://sditharum.id/" class="btn btn-outline-light btn-sm">
@@ -28,7 +28,7 @@
     </footer>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 
 {{-- dark mode system --}}
