e2755bf37f43e3602ce5bdb32c61ba34102f6357
hasinilmalik <hasinilmalik@gmail.com>
2022-03-05 00:16:07 +0700

    perbaikan

--- database/seeders/SetupSeeder.php
+++ database/seeders/SetupSeeder.php
@@ -54,17 +54,11 @@ class SetupSeeder extends Seeder
                 'value'=>'                        <p>
                             1. Cek Rincian Daftar Ulang yang Tertera Sebelumnya, Jika ada yang kurang jelas bisa ditanyakan di
                             sekolah.
-                        </p>
-                        <p>
                             2. Lakukan Transfer dengan Nominal yang sesuai seperti di Rincian sebelumnya. Transfer ke No. Rek.
                             4441234007. a/n :
                             Sekolah Dasar Islam Terpadu Harapan Umat
-                        </p>
-                        <p>
                             3. Setelah melakukan transfer, Lakukan konfirmasi ke
                             nomor 081 335 122 672. dengan format sbb:
-                        </p>
-                        <p>
                             4. Batas Daftar Ulang Maksimal hingga 14 Maret 2020. bagi yang tidak melakukan daftar ulang hingga waktu
                             yang telah
                             di
--- resources/views/member/daftarulang.blade.php
+++ resources/views/member/daftarulang.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Daftar ulang')
 
 @isset($fund)
-
     @section('content')
         @php
         $total = intval($fund->gedung + $fund->perpustakaan + $fund->kegiatan + $fund->bukumedia + $fund->seragam + $fund->jilbab + $fund->ipp);
@@ -67,47 +66,16 @@
             </tbody>
         </table>
 
-        {{-- <button class="btn btn-dark tombolmodal" id="tombolmodal">
-    Alur daftar ulang
-</button> --}}
-
-        <a target="_blank" href="https://wa.me/+6285746507030" class="btn btn-dark mb-3" id="tombolmodal">
+        <a target="_blank" href="https://wa.me/+6285746507030" class="mb-3 btn btn-dark" id="tombolmodal">
             Konfirmasi pembayaran
         </a>
-
-        <div class="accordion border" id="alur">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Alur Daftar Ulang
-                    </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#alur">
-                    <div class="accordion-body">
-                        @isset($alurdaftarulang)
-                            {!! $alurdaftarulang->value !!}
-                        @endisset
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- modal -->
-        {{-- <div id="modal" class="modal">
-    <div class="modalcard">
-        <div class="modaljudul">
-            <p>Alur daftar ulang</p>
-            <i id="tutup" class="fas fa-times-circle tutup"></i>
-        </div>
         <div class="modalbody">
-
+            @isset($alurdaftarulang)
+                {!! $alurdaftarulang->value !!}
+            @endisset
         </div>
-    </div>
-</div> --}}
     @endsection
 @else
-
     @section('content')
         <div class="text-center display-5">
             <h4>Nominal daftar ulang <br> belum ditentukan oleh admin</h4>
